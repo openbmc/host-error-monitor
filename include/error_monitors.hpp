d4bc6d00a86714aead5d90907c540629d34aa749
Jason M. Bills <jason.m.bills@intel.com>
2020-09-22 13:34:33 -0700

    WIP: Move SMI monitor to new architecture
    New architecture is intended to improve flexibility for which
    signals are monitored in a system and how they are handled.
    
    New monitoring flow handles interrupts storms better by using
    the interrupt to notify of changes and start a polling loop to
    get the actual status.
    
    Change-Id: Ic000f8c8d3a1acd06a76f47484f3cd2f06670835
    Signed-off-by: Jason M. Bills <jason.m.bills@intel.com>

---
+++ include/error_monitors.hpp
@@ -0,0 +1,37 @@
+/*
+// Copyright (c) 2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+#pragma once
+#include <error_monitors/smi_monitor.hpp>
+
+#include <memory>
+
+namespace host_error_monitor::error_monitors
+{
+static std::unique_ptr<host_error_monitor::smi_monitor::SMIMonitor> smiMonitor;
+
+void startMonitors(boost::asio::io_service& io,
+                   std::shared_ptr<sdbusplus::asio::connection> conn)
+{
+    smiMonitor =
+        std::make_unique<host_error_monitor::smi_monitor::SMIMonitor>(io, conn);
+}
+
+void sendHostOn()
+{
+    smiMonitor->hostOn();
+}
+
+} // namespace host_error_monitor::error_monitors
