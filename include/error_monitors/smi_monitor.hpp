7156dc6c25b174352502de08c312329f50491463
ankita prasad <ankita.prasad@intel.com>
2021-12-29 14:22:05 +0000

    [WIP}: Handler for Smi Interrupt
    Signed-off-by: ankita prasad <ankita.prasad@intel.com>
    Change-Id: I9c5948263b84b61603b3caaae28d04782bde9982

--- include/error_monitors/smi_monitor.hpp
+++ include/error_monitors/smi_monitor.hpp
@@ -14,17 +14,20 @@
 // limitations under the License.
 */
 #pragma once
+#include "utils.hpp"
 #include <systemd/sd-journal.h>
 
 #include <error_monitors/base_gpio_poll_monitor.hpp>
 #include <host_error_monitor.hpp>
 #include <sdbusplus/asio/object_server.hpp>
+#include <boost/asio.hpp>
 
 #include <iostream>
 
 namespace host_error_monitor::smi_monitor
 {
 static constexpr bool debug = false;
+static constexpr size_t eSpiMessageSize = 1;
 
 class SMIMonitor :
     public host_error_monitor::base_gpio_poll_monitor::BaseGPIOPollMonitor
@@ -34,6 +37,85 @@ class SMIMonitor :
             host_error_monitor::base_gpio_poll_monitor::AssertValue::lowAssert;
     const static constexpr size_t smiPollingTimeMs = 1000;
     const static constexpr size_t smiTimeoutMs = 90000;
+	std::array<uint8_t, eSpiMessageSize> eSpiBuffer = {0};
+	bool smiIsRecieved = false;
+	int eSpiFd = -1;
+	boost::asio::steady_timer pollingTimer;
+    std::chrono::steady_clock::time_point timeoutTime;
+	std::unique_ptr<boost::asio::posix::stream_descriptor> eSpiDev = nullptr;
+	
+	
+	void startPolling() override
+	{
+		constexpr const char* eSpidevName = "/dev/espi-smi"; 
+		eSpiFd = open(eSpidevName, O_RDONLY | O_NONBLOCK);
+		if (eSpiFd < 0)
+		{
+			throw std::runtime_error("Couldn't open eSPI dev");
+		}
+		eSpiDev = std::make_unique<boost::asio::posix::stream_descriptor>(io, eSpiFd);
+
+        if constexpr (debug)
+        {
+            std::cerr << "Polling " << signalName << "\n";
+        } 
+		asyncReadeSpi();
+	}
+	
+	void asyncReadeSpi(void)
+	{
+		std::cerr << "Testing SMIInterrupt... " << "\n";
+		boost::asio::async_read(
+            *eSpiDev, boost::asio::buffer(eSpiBuffer, eSpiBuffer.size()),
+        boost::asio::transfer_exactly(1),
+            [this](const boost::system::error_code& ec, size_t rlen) {
+                if (ec || rlen < 1)
+                {
+                    channelAbort(io, "Failed to read eSPI", ec);
+                    return;
+                }
+               uint8_t eSpiVal = std::get<0>(eSpiBuffer);
+			   if (eSpiVal == '1')
+			   {
+				   std::cerr << "SMIInterrupt " << "\n";
+				   timeoutTime = std::chrono::steady_clock::now() +
+                      std::chrono::duration<int, std::milli>(smiTimeoutMs);
+				   while(eSpiVal == '1' && timeoutTime > std::chrono::steady_clock::now())
+				   {
+					   boost::asio::read(*eSpiDev, boost::asio::buffer(eSpiBuffer, eSpiBuffer.size()));
+					   eSpiVal = std::get<0>(eSpiBuffer);
+					   pollingTimer.expires_after(std::chrono::milliseconds(smiPollingTimeMs));
+					   pollingTimer.async_wait([this](const boost::system::error_code ec) {
+            if (ec)
+            {
+                // operation_aborted is expected if timer is canceled before
+                // completion.
+                if (ec != boost::asio::error::operation_aborted)
+                {
+                    std::cerr << signalName
+                              << " polling async_wait failed: " << ec.message()
+                              << "\n";
+                }
+                return;
+            }
+            //poll();
+        });
+				   }
+				if(timeoutTime < std::chrono::steady_clock::now())
+				{
+					assertHandler();
+				}
+	               
+			   }
+			   
+			   else
+			   {
+				   std::cerr << "!SMIInterrupt " << "\n";
+			   }
+                asyncReadeSpi();
+            });
+		
+	}
 
     void logEvent() override
     {
@@ -62,7 +144,7 @@ class SMIMonitor :
                     return;
                 }
 #ifdef HOST_ERROR_CRASHDUMP_ON_SMI_TIMEOUT
-                startCrashdumpAndRecovery(conn, *reset, "SMI Timeout");
+                startCrashdumpAndRecovery(io, conn, *reset, "SMI Timeout");
 #else
                 if (*reset)
                 {
@@ -80,7 +162,7 @@ class SMIMonitor :
   public:
     SMIMonitor(boost::asio::io_service& io,
                std::shared_ptr<sdbusplus::asio::connection> conn,
-               const std::string& signalName) :
+               const std::string& signalName) :pollingTimer(io),
         BaseGPIOPollMonitor(io, conn, signalName, assertValue, smiPollingTimeMs,
                             smiTimeoutMs)
 
