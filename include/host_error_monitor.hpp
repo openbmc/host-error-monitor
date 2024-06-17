openbmc/host-error-monitor

commit 715fa65b1cb65e8fa10c4fa17034ef2aeca5b846

Handle recovery if crashdump fails to start
When crashdump fails to start, we still need to recover the system, so
handle recovery and delete the match in that case.

Tested:
Disabled crashdump and confirmed that the system still recovers when
crashdump fails to start.

Signed-off-by: Jason M. Bills <jason.m.bills@linux.intel.com>
Change-Id: I754b501b459bd27d7df2a8610393ab81eca1224e

include/host_error_monitor.hpp
@@ -130,6 +130,8 @@ static inline void startCrashdumpAndRecovery(
                 }
 
                 std::cerr << "failed to start Crashdump\n";
+                handleRecovery(recovery, conn);
+                crashdumpCompleteMatch.reset();
             }
         },
         "com.intel.crashdump", "/com/intel/crashdump",

commit a6895608d542a3ad8bfd249a774877d0a2dcf43c

Add missing capture
Commit 715fa65b1cb65e8fa10c4fa17034ef2aeca5b846 introduced a build error
from a missing lambda capture.

This captures "conn" to fix the build.

Signed-off-by: Jason M. Bills <jason.m.bills@linux.intel.com>
Change-Id: I6439d97489dfefa205b5da61d74434cb924b0c84

include/host_error_monitor.hpp
@@ -119,7 +119,7 @@ static inline void startCrashdumpAndRecovery(
     }
 
     conn->async_method_call(
-        [](boost::system::error_code ec) {
+        [conn](boost::system::error_code ec) {
             if (ec)
             {
                 if (ec.value() == boost::system::errc::device_or_resource_busy)